434faa50a7ae506d144bc6f1d630d3da85d6151c
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-22 04:26:17 +0900

    UserCard実装

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,23 @@ import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
+const user = {
+  name: "あああ",
+  image:
+    "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469",
+  email: "1234@example.com",
+  phone: "000-111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
+
 export const App = () => {
   return (
     <div className="App">
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 };
--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,18 +1,37 @@
-export const UserCard = () => {
+import styled from "styled-components";
+
+export const UserCard = (props) => {
+  const { user } = props;
   return (
-    <div>
-      <img alt="プロフィール" />
-      <p>名前</p>
-      <dl>
+    <SDiv>
+      <img height={160} width={160} src={user.image} alt={user.name} />
+      <p>{user.name}</p>
+      <SDl>
         <dt>メール</dt>
-        <dd>111@aaa.com</dd>
+        <dd>{user.email}</dd>
         <dt>TEL</dt>
-        <dd>000-111-222</dd>
+        <dd>{user.phone}</dd>
         <dt>会社名</dt>
-        <dd>aaa株式会社</dd>
+        <dd>{user.company.name}</dd>
         <dt>WEB</dt>
-        <dd>www.com</dd>
-      </dl>
-    </div>
+        <dd>{user.website}</dd>
+      </SDl>
+    </SDiv>
   );
 };
+
+const SDiv = styled.div`
+  text-align: left;
+  margin-top: 50px;
+`;
+
+const SDl = styled.dl`
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+  }
+`;
