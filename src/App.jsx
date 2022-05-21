aa81fc58ba8fa59261aa4c1b62cdcff8cc88e9f9
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-22 03:14:48 +0900

    usercard(organisms作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
 export const App = () => {
   return (
     <div className="App">
-      <SearchInput>検索</SearchInput>
+      <SearchInput />
+      <UserCard />
     </div>
   );
 };
---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-111-222</dd>
+        <dt>会社名</dt>
+        <dd>aaa株式会社</dd>
+        <dt>WEB</dt>
+        <dd>www.com</dd>
+      </dl>
+    </div>
+  );
+};
