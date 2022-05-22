b4b48f05509075d237b6575e90f2b30b918799ff
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-22 14:43:13 +0900

    Header作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
 
 const user = {
@@ -18,9 +19,9 @@ const user = {
 
 export const App = () => {
   return (
-    <div className="App">
+    <HeaderOnly>
       <SearchInput />
       <UserCard user={user} />
-    </div>
+    </HeaderOnly>
   );
 };
