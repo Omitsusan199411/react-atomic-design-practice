e1802e07b7632fe9d55e81b01076d37fd6eb28be
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-22 20:03:17 +0900

    pagesを作成 TopページとUsersページを作成する。

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-componets";
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>Topページ</h2>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;
