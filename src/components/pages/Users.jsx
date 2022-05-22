a41ee9a3c4be50d0089b812c7fa1ff23eebff503
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-22 21:01:45 +0900

    UserCardのidに+1

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -4,8 +4,8 @@ import { UserCard } from "../organisms/user/UserCard";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
-    id: val,
-    name: `aaa-${val}`,
+    id: `${val + 1}`,
+    name: `aaa-${val + 1}`,
     image:
       "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469",
     email: "1234@example.com",
