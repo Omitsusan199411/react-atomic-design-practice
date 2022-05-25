71be066d6bca4061a15b60a848b58e9c49c2b328
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-25 23:27:17 +0900

    memo化で不要な再レンダリングを防ぐ

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,8 +1,10 @@
+import React, { memo } from "react";
 import { Card } from "../../atoms/card/Card";
 import { UserIconWithName } from "../../molecules/user/UserIconWithName";
 import styled from "styled-components";
 
-export const UserCard = (props) => {
+export const UserCard = memo((props) => {
+  console.log("UserCard");
   const { user } = props;
   return (
     <Card>
@@ -19,7 +21,7 @@ export const UserCard = (props) => {
       </SDl>
     </Card>
   );
-};
+});
 
 const SDl = styled.dl`
   margin-bottom: 0px;
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,10 @@
-import styled from "styled-components";
+import React, { useContext } from "react";
+import { UserContext } from "../../providers/UserProvider";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+
+import styled from "styled-components";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -18,10 +22,16 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const onClickSwitch = () => {
+    setUserInfo({ isAdmin: !userInfo.isAdmin });
+  };
   return (
     <SContainer>
       <h2>Usersページ</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
       <SUserArea>
         {users.map((obj) => (
           <UserCard key={obj.id} user={obj} />
